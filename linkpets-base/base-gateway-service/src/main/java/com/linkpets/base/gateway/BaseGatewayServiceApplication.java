0cd03fdb43510fa8eb4aedb739a15ba9be0f0755
lookluke <haojie_look@163.com>
2019-10-31 18:25:27 +0800

    增加cloud-gateway模块

---
+++ linkpets-base/base-gateway-service/src/main/java/com/linkpets/base/gateway/BaseGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.linkpets.base.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ linkpets-base/base-gateway-service/src/test/java/com/linkpets/base/gateway/BaseGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkpets.base.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
